42b1afbc287290f7a5cb447317e497b4bd7409c2
MarcinDraus <marcin.drauss@gmail.com>
2024-01-21 20:15:24 +0100

    Wyszukaj element w tablicy po ID i Zwróć znaleziony element w formie JSON

--- server.js
+++ server.js
@@ -2,10 +2,32 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const db = [
+    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+  ];
+
 app.get('/', (req, res) => {
     res.send('express server');
+});
+
+app.get('/testimonials', (req, res) => {
+    res.json(db);
+})
+app.get('/testimonials/:id', (req, res) => {
+    const xid = parseInt(req.params.id);
+    const result = db.find(item => item.id === xid);
+    if (result) {
+        res.json(result);
+    } else {
+        res.status(404).json({ error: 'Not found'})
+    }
 });
 
 app.listen(port, () => {
    console.log(`Server is running on port ${port}`)
 });
+
