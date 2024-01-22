88278a419dee66042f7ddcb155f143cd0f5ad34f
MarcinDraus <marcin.drauss@gmail.com>
2024-01-22 14:28:53 +0100

    get random - zwraca losowy element tablicy

--- server.js
+++ server.js
@@ -16,16 +16,72 @@ app.get('/', (req, res) => {
 
 app.get('/testimonials', (req, res) => {
     res.json(db);
-})
+});
+
+app.get('/testimonials/random', (req, res) => {
+    console.log('Request to /testimonials/random');
+
+    const randomIndex = Math.floor(Math.random() * db.length);
+    console.log('Random index:', randomIndex);
+
+    const randomTestimonial = db[randomIndex];
+    console.log('Random testimonial:', randomTestimonial);
+
+    if (randomTestimonial) {
+        res.json(randomTestimonial);
+    } else {
+        res.status(404).json({ error: 'Not found' });
+    }
+});
+
 app.get('/testimonials/:id', (req, res) => {
-    const xid = parseInt(req.params.id);
-    const result = db.find(item => item.id === xid);
+    const id = parseInt(req.params.id);
+    console.log('Request to /testimonials/:id with id:', id);
+
+    const result = db.find(item => item.id === id);
     if (result) {
         res.json(result);
     } else {
-        res.status(404).json({ error: 'Not found'})
+        res.status(404).json({ error: 'Not found id'})
     }
 });
+
+
+// app.get('/testimonials/random', (req, res) => {
+//     console.log('Request to /testimonials/random');
+
+//     const randomIndex = Math.floor(Math.random() * db.length);
+//     console.log('Random index:', randomIndex);
+
+//     const randomTestimonial = db[randomIndex];
+//     console.log('Random testimonial:', randomTestimonial);
+
+//     if (randomTestimonial) {
+//         res.json(randomTestimonial);
+//     } else {
+//         res.status(404).json({ error: 'Not found random' });
+//     }
+// });
+
+// app.get('/testimonials/:id', (req, res) => {
+//     const xid = parseInt(req.params.id);
+
+//     // Dodaj sprawdzenie, czy xid jest prawidłową liczbą
+//     if (!isNaN(xid)) {
+//         const result = db.find(item => item.id === xid);
+//         if (result) {
+//             res.json(result);
+//         } else {
+//             res.status(404).json({ error: 'Not found id' });
+//         }
+//     } else {
+//         res.status(400).json({ error: 'Invalid id format' });
+//     }
+// });
+
+
+
+
 
 app.listen(port, () => {
    console.log(`Server is running on port ${port}`)
