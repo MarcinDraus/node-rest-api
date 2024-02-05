152914917c78b9ab7f9d6f93220e557336b07646
MarcinDraus <marcin.drauss@gmail.com>
2024-02-05 21:36:53 +0100

    connecting the react frontend to the backend and configuring the 'cors' server

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+
+// const corsOptions = {
+//    origin: 'http://localhost:3000', // Adres front-endu
+//    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//    credentials: true, // Włącz obsługę ciasteczek (jeśli korzystasz z sesji)
+//    optionsSuccessStatus: 204,
+//  };
+ 
+//  app.use(cors(corsOptions));
+ 
+
 const port = process.env.PORT || 8000;
 const { v4: uuidv4 } = require('uuid');
 const id = uuidv4();
 const db = require('./db');
-//const router = express.Router();
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const testimonialsConcerts = require('./routes/concerts.routes');
-const testimonialsSeats = require('./routes/seats.routes')
+const testimonialsSeats = require('./routes/seats.routes');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', testimonialsRoutes);
